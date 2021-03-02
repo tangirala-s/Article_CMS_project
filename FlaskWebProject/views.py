3829d7a64833d3b88539ba206764f7ba32d43865
Srivatsava Tangirala <srivatsava21@gmail.com>
2021-03-02 13:29:53 +1100

    test1

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,7 +71,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-        app.logger.error('successful Login')
+        app.logger.warning('successful Login')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
